bd71157810465dbcbb19acb0d366e24f180c1b76
Nate <nawsterfeld@outlook.com>
2019-11-03 00:13:07 -0400

    update CuratedPodcasts to be responsive for desktop/laptop

--- podcastapp-SPA/src/components/CuratedPodcasts/CuratedPodcasts.js
+++ podcastapp-SPA/src/components/CuratedPodcasts/CuratedPodcasts.js
@@ -1,31 +1,33 @@
-import React from 'react';
-import PodcastCard from '../PodcastCard/PodcastCard';
-import './CuratedPodcasts.scss';
-import { useSpring, animated } from 'react-spring';
-
+import React from 'react'
+import PodcastCard from '../PodcastCard/PodcastCard'
+import './CuratedPodcasts.scss'
+import { useSpring, animated } from 'react-spring'
 
 const CuratedPodcasts = ({ curatedListObj, getEpisodes, loading }) => {
-    const fade = useSpring({from: {opacity: 0}, opacity: 1});
-    const normal = useSpring({from: {opacity: 1}, opacity: 1});
-    
-    return (
-        <animated.div className="podcast-container" style={loading ? normal : fade}>
-            <h1 className="title">{curatedListObj.title}</h1> {
-                curatedListObj.podcasts.map(podcast => {
-                    return <PodcastCard 
-                                key={podcast.id}
-                                id={podcast.itunes_id}
-                                title={podcast.title}
-                                image={podcast.thumbnail}
-                                publisher={podcast.publisher}
-                                website={podcast.website}
-                                getEpisodes={getEpisodes}
-                                margin={20}
-                    />
-                })
-            }
-        </animated.div>
-    )
+	const fade = useSpring({ from: { opacity: 0 }, opacity: 1 })
+	const normal = useSpring({ from: { opacity: 1 }, opacity: 1 })
+
+	return (
+		<animated.div className='curated-container'>
+			<h1 className='title'>{curatedListObj.title}</h1>
+			<div className='podcast-container' style={loading ? normal : fade}>
+				{curatedListObj.podcasts.map(podcast => {
+					return (
+						<PodcastCard
+							key={podcast.id}
+							id={podcast.itunes_id}
+							title={podcast.title}
+							image={podcast.thumbnail}
+							publisher={podcast.publisher}
+							website={podcast.website}
+							getEpisodes={getEpisodes}
+							margin={20}
+						/>
+					)
+				})}
+			</div>
+		</animated.div>
+	)
 }
 
-export default CuratedPodcasts;
+export default CuratedPodcasts
