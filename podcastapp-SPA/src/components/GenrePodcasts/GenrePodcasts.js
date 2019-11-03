ee111084fdf3385c9f5c2dd1b0be966af189800e
Nate <nawsterfeld@outlook.com>
2019-11-02 23:56:48 -0400

    update GenrePodcasts to be responsive for desktop/laptop

--- podcastapp-SPA/src/components/GenrePodcasts/GenrePodcasts.js
+++ podcastapp-SPA/src/components/GenrePodcasts/GenrePodcasts.js
@@ -3,25 +3,27 @@ import PodcastCard from '../PodcastCard/PodcastCard';
 
 const GenrePodcasts = ({ genrePodcasts, getEpisodes }) => {
     return (
-        <div className="podcast-container">
+        <div className="genre-container">
             <h1 className="title">{genrePodcasts.name}</h1>
-            {
-                genrePodcasts.podcasts.map(genrePodcast => {
-                    return <PodcastCard 
-                        key={genrePodcast.id}
-                        id={genrePodcast.collectionId}
-                        title={genrePodcast.collectionName}
-                        image={genrePodcast.artworkUrl600}
-                        publisher={genrePodcast.artistName}
-                        feedUrl={genrePodcast.feedUrl}
-                        trackCount={genrePodcast.trackCount}
-                        genreIds={genrePodcast.genreIds}
-                        genres={genrePodcast.genres}
-                        getEpisodes={getEpisodes}
-                        margin={20}
-                    />
-                })
-            }
+                <div className="podcast-container">
+                {
+                    genrePodcasts.podcasts.map(genrePodcast => {
+                        return <PodcastCard 
+                            key={genrePodcast.id}
+                            id={genrePodcast.collectionId}
+                            title={genrePodcast.collectionName}
+                            image={genrePodcast.artworkUrl600}
+                            publisher={genrePodcast.artistName}
+                            feedUrl={genrePodcast.feedUrl}
+                            trackCount={genrePodcast.trackCount}
+                            genreIds={genrePodcast.genreIds}
+                            genres={genrePodcast.genres}
+                            getEpisodes={getEpisodes}
+                            margin={20}
+                        />
+                    })
+                }
+            </div>
         </div>
     )    
 }
